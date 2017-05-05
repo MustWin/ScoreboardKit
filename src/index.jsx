9ec948a622266913a51aa3006053789d4ec3ff33
Gonzalo Maldonado <elg0nz@gmail.com>
2017-05-05 09:52:50 -0700

    Turn Main App into a base component

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,24 @@
 // @flow
 
-import React from "react";
-import ReactDOM from "react-dom";
-const MainApp = () => (
-  <h1>Hello World</h1>
-);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// TODO: Remove these Anys
+class MainApp extends React.Component<any, any, any> {
+  constructor(props: any) {
+      super(props);
+      this.state = {
+          secondsWorked: 1233000,
+          secondsWorkedTodayByHour: [120000, 0, 0, 0, 0, 0]
+      };
+  }
+
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+
+}
 
 ReactDOM.render(<MainApp />, document.getElementById('app')); // eslint-disable-line
