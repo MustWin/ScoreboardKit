6cf0a2f8819cbbfedd9c30a1ac791d97234e97e5
Tenji Tembo <tenji240@gmail.com>
2017-05-08 10:40:41 -0400

    pushing props to the wrong component

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,27 @@ import DataCharts from './DataCharts/datacharts';
 class App extends React.Component {
   constructor(props){
     super(props);
+
+    this.state = {
+      currentProject: 1
+    };
+
+    this.handleCurrentProject = this.handleCurrentProject.bind(this);
+  }
+
+  handleCurrentProject(projectId) {
+    this.setState({
+      currentProject: projectId
+    });
   }
 
   render() {
+
     return(
       <div>
-        <NavBar />
+        <NavBar data={this.props.data}  />
         <Timer />
-        <DataCharts />
+        <DataCharts  />
       </div>
     );
   }
