7a57ea8a8f0bc0c24c01e88735e26691f88bb72f
Gonzalo Maldonado <elg0nz@gmail.com>
2017-05-10 09:53:33 -0700

    Add first test to Stopwatch

--- src/timer/stopwatch.jsx
+++ src/timer/stopwatch.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import IconMenu from 'material-ui/IconMenu';
 import IconButton from 'material-ui/IconButton';
@@ -6,22 +6,23 @@ import MenuItem from 'material-ui/MenuItem';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 class Stopwatch extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <IconMenu
-         iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-         anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-         targetOrigin={{horizontal: 'right', vertical: 'top'}}>
-         <MenuItem primaryText="Start" />
-         <MenuItem primaryText="Pause" />
-         <MenuItem primaryText="Stop" />
-         <MenuItem primaryText="Reset" />
-       </IconMenu>
+          iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+          anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+          targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+        >
+          <MenuItem primaryText="Start" />
+          <MenuItem primaryText="Pause" />
+          <MenuItem primaryText="Stop" />
+          <MenuItem primaryText="Reset" />
+        </IconMenu>
         <h1>00:00:00</h1>
       </div>
     );
