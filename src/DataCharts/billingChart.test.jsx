e1c4476642bd4c8ed28dbd4cef8aa3865c135863
Tenji Tembo <tenji240@gmail.com>
2017-05-16 16:03:23 -0400

    billing chart tests

---
+++ src/DataCharts/billingChart.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { getMuiTheme } from 'material-ui/styles/index';
+import BillingChart from './billingChart';
+import Line from 'react-chartjs-2';
+
+const muiTheme = getMuiTheme();
+const mountWithContext = node => (node, {
+  context: {muiTheme},
+  childContextTypes: {muiTheme: React.PropTypes.object.isRequired},
+});
+
+describe('Billing Charts', () => {
+  it('should render without throwing an error', () => {
+    expect(shallow(<BillingChart />)/contains(<Line />)).tobe(true);
+  });
+});
