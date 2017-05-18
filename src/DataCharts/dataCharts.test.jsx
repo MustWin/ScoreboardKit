586998527b9d057f1d5044dc7bc21c7585a1b745
Tenji Tembo <tenji240@gmail.com>
2017-05-18 12:25:35 -0400

    mock data

--- src/DataCharts/dataCharts.test.jsx
+++ src/DataCharts/dataCharts.test.jsx
@@ -11,7 +11,22 @@ const mountWithContext = node => (node, {
 });
 
 describe('Billing Charts', () => {
+  const project = {
+    id: 1,
+    name: 'Test Proj',
+    description: 'small description of test project',
+    billing: {
+      days: ['1','2','3','4'],
+      hours: [1,2,3,4]
+    },
+  };
+
+  const filter = '3 Days';
+
+
   it('should render without throwing an error', () => {
-    expect(shallow(<DataCharts />).contains(<Card />)).toBe(true);
+    expect(shallow(<DataCharts project={project} chartFilter={filter} />)
+      .contains(<Card />))
+      .toBe(true);
   });
 });
